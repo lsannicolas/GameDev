3cbd0c442eaac37c98786c50e5efee6c95dc3e70
Brandon-Soledad <brandonsoledad7@hotmail.com>
2021-02-17 16:32:49 -0800

    removed unused code

--- ninja.js
+++ ninja.js
@@ -25,7 +25,6 @@ class Ninja {
         this.visualRadius = 100;
         this.hp = 1000;
         this.maxHP = 1000;
-        this.healthbar = new HPBar(this);
         this.elapsedTime = 0;
         this.attackTime = 0;
         this.throwTime = 0;
@@ -392,7 +391,6 @@ class Ninja {
                 ctx.strokeRect(this.ABB.x, this.ABB.y - this.game.camera.y, this.ABB.width, this.ABB.height);
             }
         }
-        //this.healthbar.draw(ctx);
 
     };
 }
